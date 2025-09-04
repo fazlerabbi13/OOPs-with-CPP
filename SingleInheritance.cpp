eddca0c2b99dee576b568a23a952c521e0604d1b
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-04 23:25:58 +0600

    single inheritance.

---
+++ SingleInheritance.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Vehicle {
+public:
+    Vehicle() {
+        cout << "This is a Vehicle"<< endl;
+    }
+};
+
+// Sub class derived from a single base classes
+// class Car : public Vehicle {
+// public:
+//     Car() {
+//         cout << "This Vehicle is Car"<< endl;
+//     }
+// };
+
+int main() {
+    
+    // Creating object of sub class will
+    // invoke the constructor of base classes
+    // Car obj;
+    return 0;
+}
