dc15c54957d0489fe11af4cc9e6adc18bf0304f1
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-06 21:46:28 +0600

    parent class vehical is created.

---
+++ HybridInheritance.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Vehicle {
+public:
+    Vehicle() {
+        cout << "This is a Vehicle"<< endl;
+    }
+};
+
+// class Fare {
+// public:
+//     Fare() {
+//         cout << "Fare of Vehicle"<< endl;
+//     }
+// };
+
+// class Car : public Vehicle {
+//   public:
+//   Car() {
+//       cout << "This Vehical is a Car"<< endl;
+//   }
+// };
+
+// class Bus : public Vehicle, public Fare {
+//   public:
+//   Bus() {
+//       cout << "This Vehicle is a Bus with Fare";
+//   }
+// };
+
+int main() {
+    
+    // Creating object of sub class will
+    // invoke the constructor of base class.
+    // Bus obj2;
+    return 0;
+}
