8915a7c3fa4aba39a01c84842ce04ad02cb08ccd
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-06 21:37:37 +0600

    derived class Car is created.

--- HierarchicalInheritance.cpp
+++ HierarchicalInheritance.cpp
@@ -8,12 +8,12 @@ public:
     }
 };
 
-// class Car : public Vehicle {
-// public:
-//     Car() {
-//         cout << "This Vehicle is Car"<< endl;
-//     }
-// };
+class Car : public Vehicle {
+public:
+    Car() {
+        cout << "This Vehicle is Car"<< endl;
+    }
+};
 
 // class Bus : public Vehicle {
 // public:
