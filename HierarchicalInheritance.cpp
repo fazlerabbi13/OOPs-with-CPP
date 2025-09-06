86e891f6988ed47ffb2157dd69f3a4aa1a49a1dc
fazlerabbi13 <mohammad0132794@gmail.com>
2025-09-06 21:36:38 +0600

    parent class vehical is created.

---
+++ HierarchicalInheritance.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Vehicle {
+public:
+    Vehicle() {
+        cout << "This is a Vehicle"<< endl;
+    }
+};
+
+// class Car : public Vehicle {
+// public:
+//     Car() {
+//         cout << "This Vehicle is Car"<< endl;
+//     }
+// };
+
+// class Bus : public Vehicle {
+// public:
+//     Bus() {
+//         cout << "This Vehicle is Bus"<< endl;
+//     }
+// };
+
+int main() {
+    
+    // Creating object of sub class will
+    // invoke the constructor of base class.
+    // Car obj1;
+    // Bus obj2;
+    return 0;
+}
